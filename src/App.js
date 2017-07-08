c71c2daabd995044b1376f4a7117d901de5b7316
rob3n <roben1006@gmail.com>
2017-07-08 23:34:08 +0300

    delete normalize on App.js

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
 import React, { Component } from "react";
 import { BrowserRouter as Router, Route } from "react-router-dom";
-import "normalize.css";
 import "./style.css";
 import "./complex-page.css";
 import "./App.css";
